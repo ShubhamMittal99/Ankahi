59102a084a7041313bb8a9d1a851d25e52f8b4d7
TheRealXster <akarshsharma5620@gmail.com>
2019-06-30 02:25:46 +0530

    dont-know

---
+++ Final/Reminiscence/PopUp.js
@@ -0,0 +1,7 @@
+        console.log("what");
+        
+        let items = document.querySelectorAll('.item');
+        items.forEach(item => item.addEventListener('mouseenter',showDescription));
+
+        function showDescription(e){}
+        
--- Final/Reminiscence/TopUp.js
+++ Final/Reminiscence/TopUp.js
@@ -1,7 +1,6 @@
-        console.log("what");
-        
-        let items = document.querySelectorAll('.item');
-        items.forEach(item => item.addEventListener('mouseenter',showDescription));
-
-        function showDescription(e){}
+/*jslint devel:true */      
+/*console.log("what");
+let items = document.querySelectorAll('.item');  
+items.forEach(item => item.addEventListener('mouseenter',showDescription));
+  function showDescription(e){    }*/
         
