36c772cf034b5f7b11ffce0a05c6fc9a1e798925
TheRealXster <akarshsharma5620@gmail.com>
2019-07-02 16:58:59 +0530

    clicking outside description

--- Final/Reminiscence/TopUp.js
+++
@@ -1,6 +0,0 @@
-/*jslint devel:true */      
-/*console.log("what");
-let items = document.querySelectorAll('.item');  
-items.forEach(item => item.addEventListener('mouseenter',showDescription));
-  function showDescription(e){    }*/
-        
