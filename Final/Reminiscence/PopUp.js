3cad9cfe629bea7fc473aabc50a5787cba91bc2e
Bibiliophilia <shubhammittal1810@gmail.com>
2019-07-07 21:17:36 +0530

    productions started

--- Final/Reminiscence/PopUp.js
+++
@@ -1,7 +0,0 @@
-        console.log("what");
-        
-        let items = document.querySelectorAll('.item');
-        items.forEach(item => item.addEventListener('mouseenter',showDescription));
-
-        function showDescription(e){}
-        
